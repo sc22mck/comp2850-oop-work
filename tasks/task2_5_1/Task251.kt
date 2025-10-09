a9a9002cfdce6dba27cf91128ba040d42e3f6b32
sc22mck <124677707+sc22mck@users.noreply.github.com>
2025-10-09 15:52:46 +0000

    Did the tasks for the first two chapters of kotlin essentials.

---
+++ tasks/task2_5_1/Task251.kt
@@ -0,0 +1,6 @@
+fun main() {
+ var myPin = 1234
+  println("myPin")
+  var herPin = 4321
+  println("herPinPin")
+}
---
+++ tasks/task2_5_2/task252.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val myAge = 29u
+    println(myAge::class)
+    val universeAge = 13_800_000_0001
+    println(universeAge::class)
+    val status = 'M'
+    println(status::class)
+    val name = "Sarah"
+    println(name::class)
+    val height = 1.78f
+    println(name::height)
+    val root2 = Math.sqrt(2.0)
+    println(root2::class)
+}
