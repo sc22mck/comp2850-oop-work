b056a7981ebf08a09eaefd610f77486ee61ed5cf
sc22mck <124677707+sc22mck@users.noreply.github.com>
2025-10-20 11:31:19 +0000

    Finished the first tasks on functions.

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,17 @@
 // Task 5.1.1: anagram checking using a function
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false
+    }
+    val firstChars = first.lowercase().toList().sorted()
+    val secondChars = second.lowercase().toList().sorted()
+    return firstChars == secondChars
+}
+
+fun main() {
+    println("Enter your first anagram: ")
+    val anag1 = readLine() ?: ""
+    println("Enter your second anagram: ")
+    val anag2 = readLine() ?: ""
+  println(anagrams(anag1, anag2))
+}
--- tasks/task5_1_2/Die.kt
+++ tasks/task5_1_2/Die.kt
@@ -1,1 +1,24 @@
 // Task 5.1.2: die rolling simulation
+import kotlin.random.Random
+
+fun rollDie(sides: Int) {
+    if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+        println("Rolling a d$sides...")
+        val result = Random.nextInt(1, sides + 1)
+        println("You rolled $result")
+    }
+    else {
+        println("Error: cannot have a $sides-sided die")
+    }
+}
+
+fun readInt(xprompt: String): Int {
+    print(xprompt)
+    val userInput = readLine()
+    return userInput!!.toInt()
+}
+
+fun main() {
+    val sides = readInt("Enter the number of sides for the die: ")
+    rollDie(sides)
+}
