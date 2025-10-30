fa477c4bfe22bc98504cbc3af8158c176d85a8f3
sc22mck <124677707+sc22mck@users.noreply.github.com>
2025-10-30 10:38:28 +0000

    Done some of the wordle assignment

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,2 +1,13 @@
 fun main() {
+    println("Welcome to Wordle")
+    println("Try Guess the 5 letter word and you can have up to 10 attempts,")
+    println("'?' indicates incorrect letters.")
+
+    Try {
+        val words = readWordList("data/words.txt")
+
+        if (words.isEmpty()) 
+        println*"Error: No words found in the word list.")
+        return
+    }
 }
--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -2,11 +2,51 @@
 
 //A function to check that the provided word is 5 letters long
 fun isValid(word : String): Boolean {
-    if (word.length != 5){
-    return false}
+    return word.length == 5 && word.all { it.isLetter}
 }
 
 // Reads words from a specified file and returns them as strings
 fun readWordList(filename : String) MutableList <String>{
-
+    return File(filename).readLines().map { it.trim() }.toMutableList()
 }
+
+//Choose a random word from the given list
+fun pickRandomWord(words: MutableList<String>): String {
+    if (words.isEmpty()) {
+        throw IllegalStateException("Word List empty")
+    }
+    val randomIndex = (0 until words.size).random()
+    return word.removeAt(randomIndex)
+}
+
+//Prints a prompt using given number attepmt
+fun obtainGuess(attempt: Int): String {
+    while(true) {
+        print("Attempt $attempt: ")
+        val(guess = readLines()?.trim()?.lowercase()
+        if(guess != null && isValid(guess)) {
+            return guess
+        } else {
+            println("Invalid guess attempt!!! Please enter a 6-letter word")
+        }
+    }
+}
+//Compare guess word to target
+fun evaluateGuess(guess: String, target: String): List<Int> {
+    require(guess.length == 5 && target.length == 5) {
+        "Both targets must be 5-letter words
+    }
+    return guess.mapIndexed { index, char ->
+    if (char == target[index]) 1 else 0
+    }
+}
+//  Displays the letters of a gues that match target word
+ fun displayGuess(gues: String, matches: List<Int>) {
+    require(guess.length == 5 && matches.sizes == 5) { "Guess must be 5 letters and matches must have 5 elements"}
+
+    val display = guess.mapIndexed { index, char ->
+        if (matches[index] == 1) char else '?'
+        }.joinToString("")
+
+        println(display)
+ }
