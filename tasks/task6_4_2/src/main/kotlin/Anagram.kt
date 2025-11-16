46927ff314d0c7c7f4d6f30c37b1dc4ce676a98c
sc22mck <124677707+sc22mck@users.noreply.github.com>
2025-11-16 17:47:48 +0000

    Task 6.2 completed on Kotests 3

--- tasks/task6_4_2/src/main/kotlin/Anagram.kt
+++ tasks/task6_4_2/src/main/kotlin/Anagram.kt
@@ -2,6 +2,7 @@
 
 infix fun String.anagramOf(other: String) = when {
     this.length != other.length -> false
+    this.isEmpty() -> false // Empty strings must return false as they can't be anagrams of each other
     else -> {
         val theseChars = this.lowercase().toList().sorted()
         val otherChars = other.lowercase().toList().sorted()
--- tasks/task6_4_2/src/test/kotlin/AnagramTest.kt
+++ tasks/task6_4_2/src/test/kotlin/AnagramTest.kt
@@ -8,6 +8,32 @@ import io.kotest.matchers.shouldBe
 class AnagramTest: StringSpec({
     // Write your unit tests here!
     //A test to check the string lenghth for both anagrams is equal
-  
-    }
+   "Anagrams must have an equal length" {
+    "Anagrams".anagramOf("Anagram") shouldBe false
+    "abc".anagramOf("abcd") shouldBe false
+    "Hot".anagramOf("Hotter") shouldBe false
+    "".anagramOf("xyz") shouldBe false
+   }
+   "An empty string isn't an anagram of itself"{
+    "".anagramOf("") shouldBe false
+   }
+   "A non-empty string is an anagram of itself" {
+    "helloworld".anagramOf("helloworld") shouldBe true
+    "Hannah".anagramOf("Hannah") shouldBe true
+    "True".anagramOf("True") shouldBe true
+    "False".anagramOf("False") shouldBe true
+   }
+   "Two Strings are anagrams if they have the same characters in different orders." {
+    "tea".anagramOf("eat") shouldBe true
+    "elvis".anagramOf("lives") shouldBe true
+    "arm".anagramOf("ram") shouldBe true
+    "bard".anagramOf("brad") shouldBe true
+   }
+   "Lowercase and Uppercase forms of a character are equivalent"{
+    "helloworld".anagramOf("HelLoWORLD") shouldBe true
+    "true".anagramOf("TRUE") shouldBe true
+    "TEST".anagramOf("test") shouldBe true
+    "Fire".anagramOf("fIre") shouldBe true
+   }
+    
 })
